10a16507cce7a069ed96186fb869c004bbd9e566
DESKTOP-SN0LLQ6\marwa <marwangamal931@gmail.com>
2026-06-29 00:50:38 +0300

    Revert "intial commit"
    This reverts commit bd61c8488322525ee68fe44dddb93dcf3a8a89ad.

--- android/app/src/main/kotlin/com/example/space_app/space_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.space_app.space_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/utilites/app_colors.dart
+++
@@ -1,8 +0,0 @@
-// import 'dart:ui';
-import 'package:flutter/material.dart';
-
-abstract final class AppColors {
-  static const black = Color(0xff0E0E0E);
-  static const white = Color(0xffffffff);
-  static const red = Color(0xffEE403D);
-}
